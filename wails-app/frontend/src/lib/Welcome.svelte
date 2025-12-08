ef4fdc06f1f22d6171f7a6cddb24c65bad44f044
infraflakes <infraflakes@proton.me>
2025-12-08 23:11:22 +0700

    Change screen time title

--- wails-app/frontend/src/lib/Welcome.svelte
+++ wails-app/frontend/src/lib/Welcome.svelte
@@ -99,8 +99,8 @@
     <div class="card-body">
       <div class="d-flex justify-content-between align-items-center mb-3">
         <h5 class="card-title mb-0">
-          <i class="bi bi-clock-history me-2"></i>Thời gian sử dụng ứng dụng hôm
-          nay
+          <i class="bi bi-clock-history me-2"></i>Các ứng dụng sử dụng nhiều
+          nhất hôm nay
         </h5>
         <span class="badge fs-6 text-black" style="background-color: #f76f53;"
           >{formatDuration(totalScreenTime)}</span
